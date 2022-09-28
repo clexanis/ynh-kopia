b439467d117236cd7595a0bdff6b2b13556ccc89
clexanis <clexanis@users.noreply.github.com>
2022-09-28 18:07:16 +0200

    Merge pull request #4 from clexanis/lint
    Lint

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+: # Fix for shellcheck to work only on block instead on the whole file
 
 #=================================================
 # COMMON VARIABLES
@@ -6,13 +7,13 @@
 # App package root directory should be the parent folder
 export pkg_dependencies="wget"
 
-# Install Kopia with apt repo
+# Install Kopia with apt repository
 install_kopia_with_repo () {
 	# Check and download key if didn't exist
 	if [ ! -f /etc/apt/trusted.gpg.d/kopia.gpg ]; then
 		wget -qO - https://kopia.io/signing-key | apt-key --keyring /etc/apt/trusted.gpg.d/kopia.gpg add -
 	fi
-	# Add repo if it didn't exist
+	# Add repository if it didn't exist
 	if [ ! -f /etc/apt/sources.list.d/kopia.list ]; then
 		printf "deb http://packages.kopia.io/apt/ stable main" | tee /etc/apt/sources.list.d/kopia.list
 		apt-get update
