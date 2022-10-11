b4df058aad5f11df9171bd302ba8927a1836072f
clexanis <clexanis@users.noreply.github.com>
2022-10-11 18:09:57 +0200

    Update yunohost requirement

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,15 +4,15 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-
 # Install Kopia with apt repository
 install_kopia_with_repo () {
 	# Check and download key if didn't exist
 	ynh_install_extra_repo -r "deb http://packages.kopia.io/apt/ stable main" -k "https://kopia.io/signing-key" -n "kopia"
-	ynh_package_install kopia
+	ynh_package_install "kopia"
 }
 
+# Uninstall Kopia with apt repository
 uninstall_kopia_with_repo () {
 	ynh_remove_extra_repo -n "kopia"
-	ynh_package_autopurge kopia
+	ynh_package_autopurge "kopia"
 }
