5063828571481a5b84bbdce6c32f481bee01a844
clexanis <clexanis@users.noreply.github.com>
2022-10-11 17:50:43 +0200

    Use helpers instead of generic commands

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,25 +4,15 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-# App package root directory should be the parent folder
-export pkg_dependencies="wget"
 
 # Install Kopia with apt repository
 install_kopia_with_repo () {
 	# Check and download key if didn't exist
-	if [ ! -f /etc/apt/trusted.gpg.d/kopia.gpg ]; then
-		wget -qO - https://kopia.io/signing-key | apt-key --keyring /etc/apt/trusted.gpg.d/kopia.gpg add -
-	fi
-	# Add repository if it didn't exist
-	if [ ! -f /etc/apt/sources.list.d/kopia.list ]; then
-		printf "deb http://packages.kopia.io/apt/ stable main" | tee /etc/apt/sources.list.d/kopia.list
-		apt-get update
-	fi
-	apt-get install kopia
+	ynh_install_extra_repo -r "deb http://packages.kopia.io/apt/ stable main" -k "https://kopia.io/signing-key" -n "kopia"
+	ynh_package_install kopia
 }
+
 uninstall_kopia_with_repo () {
-	apt-get purge kopia
-	rm -f /etc/apt/trusted.gpg.d/kopia.gpg
-	rm -f /etc/apt/sources.list.d/kopia.list
-	apt-get update
+	ynh_remove_extra_repo -n "kopia"
+	ynh_package_autopurge kopia
 }
